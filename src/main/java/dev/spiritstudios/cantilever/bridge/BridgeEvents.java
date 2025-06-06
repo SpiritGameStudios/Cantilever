ae8a867e390fdf58203dfa51841bd2293d957aa0
Anastasia Calico <calico@merchantcalico.net>
2025-06-06 18:47:12 +1000

    refactor: Message delay for webhook now searches future messages.

--- src/main/java/dev/spiritstudios/cantilever/bridge/BridgeEvents.java
+++ src/main/java/dev/spiritstudios/cantilever/bridge/BridgeEvents.java
@@ -2,14 +2,18 @@ package dev.spiritstudios.cantilever.bridge;
 
 import dev.spiritstudios.cantilever.Cantilever;
 import dev.spiritstudios.cantilever.CantileverConfig;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.message.v1.ServerMessageEvents;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.Util;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class BridgeEvents {
 	private static Bridge bridge;
@@ -52,6 +56,8 @@ public class BridgeEvents {
 		ServerMessageEvents.CHAT_MESSAGE.register((message, user, params) -> BridgeEvents.bridge.sendWebhookMessageM2D(message, user));
 	}
 
+	private static ScheduledExecutorService scheduler;
+
 	private static void registerDiscordEvents() {
 		BridgeEvents.bridge.api().addEventListener(new ListenerAdapter() {
 			@Override
@@ -61,14 +67,22 @@ public class BridgeEvents {
 					return;
 				}
 
-				Util.getIoWorkerExecutor().execute(() -> {
-					try {
-						// Catch all to make sure that PluralKit, Tupperbox and other webhook utilizing bots'
-						// original message does not get caught and sent.
-						Thread.sleep(CantileverConfig.INSTANCE.discordMessageDelay.get());
+				var webhooksForRemoval = CantileverConfig.INSTANCE.webhooksForRemoval.get();
+				if (scheduler == null && CantileverConfig.INSTANCE.d2mMessageDelay.get() > 0 && (!webhooksForRemoval.webhookIds().isEmpty() || webhooksForRemoval.inverted())) {
+					scheduler = Executors.newScheduledThreadPool(1, runnable -> {
+						var thread = new Thread(runnable, "Cantilever D2M Message Scheduler");
+						thread.setDaemon(true);
+						thread.setUncaughtExceptionHandler((thread1, throwable) -> Cantilever.LOGGER.error("Caught exception in D2M Message Scheduler", throwable));
+						return thread;
+					});
+				}
 
-						var messageHistory = event.getChannel().asTextChannel().getHistoryAround(event.getMessageIdLong(),4).complete();
-						if (messageHistory.getMessageById(event.getMessageIdLong()) == null) {
+				if (scheduler != null) {
+					var historyFuture = event.getChannel().asGuildMessageChannel().getHistoryAfter(event.getMessageIdLong(), CantileverConfig.INSTANCE.webhookMessagesToCheck.get());
+					scheduler.schedule(() -> {
+						var history = historyFuture.complete();
+						if (!history.isEmpty() && !event.isWebhookMessage() && history.getRetrievedHistory().stream()
+							.noneMatch(message -> isMessageWebhook(message, event.getMessage()))) {
 							return;
 						}
 
@@ -78,10 +92,27 @@ public class BridgeEvents {
 							)),
 							true
 						));
-					} catch (InterruptedException e) {
-						throw new RuntimeException(e);
-					}
-				});
+					}, CantileverConfig.INSTANCE.d2mMessageDelay.get(), TimeUnit.MILLISECONDS);
+					return;
+				}
+
+				BridgeEvents.bridge.sendBasicMessageD2M(new BridgeTextContent(
+					Text.of(CantileverConfig.INSTANCE.gameChatFormat.get().formatted(
+						event.getAuthor().getName(), event.getMessage().getContentDisplay()
+					)),
+					true
+				));
+			}
+
+			private static boolean isMessageWebhook(Message message, Message originalMessage) {
+				if (!message.isWebhookMessage() || message.getAuthor().getIdLong() == BridgeEvents.bridge.getWebhookId() ||
+					!originalMessage.getContentRaw().contains(message.getContentRaw())) {
+					return false;
+				}
+
+				var webhooksForRemoval = CantileverConfig.INSTANCE.webhooksForRemoval.get();
+				return webhooksForRemoval.webhookIds().stream()
+					.anyMatch(s -> s == message.getAuthor().getIdLong() ^ webhooksForRemoval.inverted());
 			}
 		});
 	}
