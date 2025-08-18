1077deab52f1d12f9b0ec12059fdf1e989e6ebbe
Anastasia Calico <calico@merchantcalico.net>
2025-08-18 22:51:48 +1000

    fix: Don't register events when bridge is null and remove warnings from BridgeEvents.

--- src/main/java/dev/spiritstudios/cantilever/bridge/BridgeEvents.java
+++ src/main/java/dev/spiritstudios/cantilever/bridge/BridgeEvents.java
@@ -2,6 +2,7 @@ package dev.spiritstudios.cantilever.bridge;
 
 import dev.spiritstudios.cantilever.Cantilever;
 import dev.spiritstudios.cantilever.CantileverConfig;
+import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -9,15 +10,17 @@ import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.message.v1.ServerMessageEvents;
 import net.minecraft.util.Identifier;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class BridgeEvents {
+	@Nullable
 	private static Bridge bridge;
 
-	public static void init(Bridge bridge) {
+	public static void init(@Nullable Bridge bridge) {
 		BridgeEvents.bridge = bridge;
 
 		registerMinecraftEvents();
@@ -25,6 +28,8 @@ public class BridgeEvents {
 	}
 
 	private static void registerMinecraftEvents() {
+		if (BridgeEvents.bridge == null)
+			return;
 		ServerLifecycleEvents.SERVER_STARTING.register(
 			Identifier.of(Cantilever.MODID, "after_bridge"),
 			server -> BridgeEvents.bridge.sendBasicMessageM2D(CantileverConfig.INSTANCE.gameEventFormat.get().formatted("Server starting..."))
@@ -43,7 +48,10 @@ public class BridgeEvents {
 
 		ServerLifecycleEvents.SERVER_STOPPED.register(server -> {
 			BridgeEvents.bridge.sendShutdownMessageM2D(CantileverConfig.INSTANCE.gameEventFormat.get().formatted("Server stopped"));
-			BridgeEvents.bridge.api().shutdownNow();
+			JDA api = BridgeEvents.bridge.api();
+			if (api != null) {
+				api.shutdownNow();
+			}
 		});
 
 		ServerMessageEvents.GAME_MESSAGE.register((server, message, overlay) -> {
@@ -62,7 +70,14 @@ public class BridgeEvents {
 	private static ScheduledExecutorService scheduler;
 
 	private static void registerDiscordEvents() {
-		BridgeEvents.bridge.api().addEventListener(new ListenerAdapter() {
+		if (BridgeEvents.bridge == null)
+			return;
+
+		JDA api = BridgeEvents.bridge.api();
+		if (api == null)
+			return;
+
+		api.addEventListener(new ListenerAdapter() {
 			@Override
 			public void onMessageReceived(@NotNull MessageReceivedEvent event) {
 				if (!BridgeEvents.bridge.channel().map(c -> c == event.getChannel()).orElse(false) ||
